c3a05f2f24338d57266f9496d9738035638e710e
fluidbyte <kent@fluidbyte.net>
2013-11-21 23:14:53 +0000

    Added config and static server

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
 var express = require("express"),
-    app = express();
+    app = express(),
+    builds = express(),
+    config = require("config.json");
+
+/**
+ * Deploy listener
+ */
 
 app.get("/:project", function(req, res){
     
-    res.send(req.params.project);
+    // Ensure the project has been config'd
+    if (!config.hasOwnProperty[req.param.project]) {
+        // Nope, send an error
+        res.send("Failed to deploy. Missing or incorrect configuration");
+    } else {
+        res.send("Deploying!");
+    }
 
 });
 
 app.listen(1337);
+console.log("Serving app over 1337");
+
+/**
+ * Static server
+ */
+
+builds.use(express.static(__dirname + '/builds'));
+builds.listen(80);
+console.log("Serving builds over 80");
