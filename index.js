09ce2e9c6b76a6cc128b2d868073bc98cc8f2d91
fluidbyte <kent@fluidbyte.net>
2013-11-22 23:41:50 +0000

    Lots of work...

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
-var express = require("express"),
+var fs = require("fs"),
+    fse = require("fs-extra"),
+    step = require("step"),
+    express = require("express"),
     app = express(),
     builds = express(),
-    spawn = require('child_process').spawn,
-    config = require("./config.json");
+    exec = require("child_process").exec,
+    git = require("gift"),
+    config = require("./config.json"),
+    build_path = __dirname + "/builds/";
 
 /**
  * Deploy listener
  */
 
-app.get("/:project", function(req, res){
+app.get("/:project", function(req, res) {
     
     // Ensure the project has been config'd
     if (!config.hasOwnProperty(req.params.project)) {
@@ -16,30 +21,47 @@ app.get("/:project", function(req, res){
         res.send("Failed to deploy. Missing or incorrect configuration");
     } else {
         
-        // Good to go
+        res.send("Deploying...");
         
-        var command = spawn(__dirname + "/runner.sh", [__dirname + "/builds/" + req.params.project.dir], {
-            cwd: __dirname + "/builds/" + req.params.project.dir    
-        });
-        var output  = [];
+        // Set build
+        var build = config[req.params.project];
         
-        command.stdout.on('data', function (chunk) {
-            output.push(chunk);
-        }); 
-        
-        command.on('close', function(code) {
-            if (code === 0) {
-                res.send("Deployed");
-                console.log(Buffer.concat(output).toString());
-            } else {
-                res.send("Error");
-                console.log("ERROR " + code + " " + Buffer.concat(output).toString());
+        step(
+            // Cleanup
+            function cleanup () {
+                console.log("Cleanup");
+                fse.remove(build_path+build.dir, this);
+            },
+            // Clone
+            function clone (err) {
+                console.log("Cloning");
+                if (err) {
+                    console.log("ERROR: cleanup");
+                }
+                git.clone(build.repo, __dirname+"/builds/"+build.dir, this);
+            },
+            // Install
+            function install (err) {
+                console.log("Installing");
+                if (err) {
+                    console.log("ERROR: clone");
+                }
+                exec("npm install", { cwd: build_path+build.dir }, this);
+            },
+            // Complete
+            function complete () {
+                if (arguments[0] !== null) {
+                    console.log("ERROR: install");
+                } else {
+                    console.log("Built!");
+                }
             }
-        });
             
+        );
     }
 
 });
+
 
 app.listen(1337);
 console.log("Serving app over 1337");
