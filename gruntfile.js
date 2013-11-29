b73b424a80b8e3d6ee1bec034b0ee28177a684c0
fluidbyte <kent@fluidbyte.net>
2013-11-29 23:29:57 +0000

    Working on distribution build

--- gruntfile.js
+++ gruntfile.js
@@ -3,21 +3,6 @@ module.exports = function ( grunt ) {
     grunt.initConfig({
         pkg: grunt.file.readJSON("package.json"),
         /**
-         * RequireJS
-         */
-        requirejs: {
-            main: {
-                options: {
-                    name: "main",
-                    baseUrl: "ui/src/js",
-                    mainConfigFile: "ui/src/js/main.js",
-                    out: "ui/dist/js/main.js",
-                    // include: ...
-                    logLevel: 2
-                }
-            }
-        },
-        /**
          * JSHint
          */
         jshint: {
@@ -40,6 +25,52 @@ module.exports = function ( grunt ) {
                 }
             }
         },
+        
+        /**
+         * RequireJS
+         */
+        requirejs: {
+            main: {
+                options: {
+                    name: "main",
+                    baseUrl: "ui/src/js",
+                    mainConfigFile: "ui/src/js/main.js",
+                    out: "ui/dist/js/main.js",
+                    // include: ...
+                    logLevel: 2
+                }
+            }
+        },
+        
+        /**
+         * Copy files into dist from src
+         */
+        copy: {
+            main: {
+                files: [
+                    // Vendor files (libraries)
+                    { expand: true, cwd: "ui/src/js/vendor/", src: [ "**" ], dest: "ui/dist/js/vendor/" },
+                    // CSS files
+                    { expand: true, cwd: "ui/src/css/", src: [ "**" ], dest: "ui/dist/css/" },
+                    // Index
+                    { expand: true, cwd: "ui/src/", src: [ "index.html" ], dest: "ui/dist/" }
+                ]
+            }
+        },
+        
+        /**
+         * Minify CSS file
+         */
+        cssmin: {
+            minify: {
+                expand: true,
+                cwd: "ui/src/css/",
+                src: ["*.css"],
+                dest: "ui/dist/css/",
+                ext: ".css"
+            }
+        },
+        
         /**
          * Watch
          */
@@ -55,11 +86,13 @@ module.exports = function ( grunt ) {
     });
     
     // Load NPM Tasks
-    grunt.loadNpmTasks("grunt-contrib-requirejs");
     grunt.loadNpmTasks("grunt-contrib-jshint");
+    grunt.loadNpmTasks("grunt-contrib-requirejs");
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-watch');
     
     // Register Tasks
-    grunt.registerTask("default", ["requirejs", "jshint"]);
+    grunt.registerTask("default", ["jshint", "requirejs", "copy", "cssmin"]);
     
 };
