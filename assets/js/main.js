669d2503534c5cb7f6c56bcc9603054b416cb76e
Mike Hostetler <mike.hostetler@gmail.com>
2014-02-24 21:47:00 -0600

    In-process of conversion to Durandal

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,22 @@
 ﻿requirejs.config({
-		baseUrl: "js",
     paths: {
         'text': 'vendor/requirejs-text/text',
         'durandal':'vendor/durandal',
         'plugins' : 'vendor/durandal/plugins',
         'transitions' : 'vendor/durandal/transitions',
         'knockout': 'vendor/knockout/knockout-2.3.0',
-        'jquery': 'vendor/jquery/jquery'
+        'jquery': 'vendor/jquery/jquery',
+				'handlebars': 'vendor/handlebars/handlebars.min',
+				'ansi_up': 'vendor/ansi_up/ansi_up',
+				'github': 'vendor/github/github',
+				'base64': 'vendor/base64/base64',
+				'async': 'vendor/async/async',
+				'underscore': 'vendor/underscore/underscore-min'
     },
     shim: {
+				'handlebars': {
+					'exports': 'Handlebars'
+				}
     }
 });
 
@@ -20,9 +28,7 @@ define(['durandal/system', 'durandal/app', 'durandal/viewLocator'],  function (s
     app.title = 'CodeSwarm Durandal!';
 
     app.configurePlugins({
-        router: true,
-        dialog: true,
-        widget: true
+        router: true
     });
 
     app.start().then(function() {
@@ -31,6 +37,7 @@ define(['durandal/system', 'durandal/app', 'durandal/viewLocator'],  function (s
         viewLocator.useConvention();
 
         //Show the app by setting the root view model for our application with a transition.
-        app.setRoot('viewmodels/shell', 'entrance');
+        //app.setRoot('viewmodels/shell', 'entrance');
+        app.setRoot('codeswarm/app');
     });
 });
