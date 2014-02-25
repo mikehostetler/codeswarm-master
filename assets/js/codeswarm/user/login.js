630599a28ecf00707d041f1c040eadc6cf5028eb
fluidbyte <kent@fluidbyte.net>
2014-02-25 22:05:07 +0000

    Setup observables for form values

--- assets/js/codeswarm/user/login.js
+++ assets/js/codeswarm/user/login.js
@@ -4,9 +4,13 @@ define([
 
 function(ko) {
     var ctor = function () {
+        var self = this;
         this.displayName = 'Login';
+        this.username = ko.observable('');
+        this.password = ko.observable('');
         this.tryLogin = function () {
-          console.log('login!');
+          console.log(self.username() + " " + self.password());
+          //console.log("Login!");
         };
     };
     return ctor;
