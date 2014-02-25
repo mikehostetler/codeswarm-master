2e700647d8f564b7b37a4619365a607b69efcb55
fluidbyte <kent@fluidbyte.net>
2014-02-25 23:24:18 +0000

    Convert to objLit, hooked up initial method, request caller and handlers pass/fail

--- assets/js/codeswarm/user/login.js
+++ assets/js/codeswarm/user/login.js
@@ -1,17 +1,47 @@
 define([
-  'knockout'
+  'knockout',
+  'jquery'
 ],
 
-function(ko) {
-    var ctor = function () {
-        var self = this;
-        this.displayName = 'Login';
-        this.username = ko.observable('');
-        this.password = ko.observable('');
-        this.tryLogin = function () {
-          console.log(self.username() + " " + self.password());
-          //console.log("Login!");
-        };
+function(ko, $) {
+    var ctor = {
+
+        // Set displayName
+        displayName: 'Login',
+
+        // Setup model
+        username: ko.observable(),
+        password: ko.observable(),
+
+        // Login handler method
+        tryLogin: function () {
+          this.request(this.username(), this.password());
+        },
+
+        // Make login request
+        loginReq: function (username, password) {
+          $.ajax({
+            url: '/session',
+            type: 'POST',
+            data: {
+              'username': username,
+              'password': password
+            },
+            success: this.loginReqPass,
+            failure: this.loginReqFail
+          });
+        },
+
+        // Request successful
+        loginReqPass: function (data) {
+          console.log(data);
+        },
+
+        // Request failure
+        loginReqFail: function (err) {
+          console.log(err);
+        }
+
     };
     return ctor;
 });
