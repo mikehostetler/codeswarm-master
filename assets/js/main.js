05f776234b4345e3302e313115e9a97214a03bfd
Mike Hostetler <mike.hostetler@gmail.com>
2014-02-24 20:18:35 -0600

    Initial integration of Durandal

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,36 @@
-require.config({
-	baseUrl: "js",
-	paths: {
-		"jquery": "vendor/jquery/jquery.min",
-		"text": "vendor/requirejs-text/text",
-		"handlebars": "vendor/handlebars/handlebars.min",
-		"ansi_up": "vendor/ansi_up/ansi_up",
-		"github": "vendor/github/github",
-		"base64": "vendor/base64/base64",
-		"async": "vendor/async/async",
-		"underscore": "vendor/underscore/underscore-min",
-		"app": "app"
-	},
-	shim: {
-		"handlebars": {
-			"exports": "Handlebars"
-		}
-	}
+﻿requirejs.config({
+		baseUrl: "js",
+    paths: {
+        'text': 'vendor/requirejs-text/text',
+        'durandal':'vendor/durandal',
+        'plugins' : 'vendor/durandal/plugins',
+        'transitions' : 'vendor/durandal/transitions',
+        'knockout': 'vendor/knockout/knockout-2.3.0',
+        'jquery': 'vendor/jquery/jquery'
+    },
+    shim: {
+    }
 });
 
-define(["app"], function (app) {
+define(['durandal/system', 'durandal/app', 'durandal/viewLocator'],  function (system, app, viewLocator) {
+    //>>excludeStart("build", true);
+    system.debug(true);
+    //>>excludeEnd("build");
 
-	//Start the application
-	app.init();
+    app.title = 'CodeSwarm Durandal!';
 
+    app.configurePlugins({
+        router: true,
+        dialog: true,
+        widget: true
+    });
+
+    app.start().then(function() {
+        //Replace 'viewmodels' in the moduleId with 'views' to locate the view.
+        //Look for partial views in a 'views' folder in the root.
+        viewLocator.useConvention();
+
+        //Show the app by setting the root view model for our application with a transition.
+        app.setRoot('viewmodels/shell', 'entrance');
+    });
 });
