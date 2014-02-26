e30069f414287f253ace0435f8b4bf96b5287117
fluidbyte <kent@fluidbyte.net>
2014-02-26 14:45:16 +0000

    Laid out logout controller structure

--- assets/js/codeswarm/user/logout.js
+++ assets/js/codeswarm/user/logout.js
@@ -1,12 +1,18 @@
-﻿define(function() {
-    var ctor = function () {
-        this.displayName = 'About CodeSwarm';
+define(function() {
+    var ctor = {
+
+        // Set displayName
+        displayName: 'Log Out',
+
+        activate: function () {
+          // Not sure on sessions/models yet,
+          // assume we just need to release all
+          // client-side stores on activate then
+          // pass the user to another route...
+        }
+
     };
 
-    //Note: This module exports a function. That means that you, the developer, can create multiple instances.
-    //This pattern is also recognized by Durandal so that it can create instances on demand.
-    //If you wish to create a singleton, you should export an object instead of a function.
-    //See the "flickr" module for an example of object export.
-
     return ctor;
+
 });
