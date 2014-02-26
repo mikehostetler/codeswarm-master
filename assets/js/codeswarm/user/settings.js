30a75adad76d64464fcfdd99a961c9e0c9183126
fluidbyte <kent@fluidbyte.net>
2014-02-26 03:29:56 +0000

    Working on requests. Running out of steam

--- assets/js/codeswarm/user/settings.js
+++ assets/js/codeswarm/user/settings.js
@@ -10,11 +10,6 @@ function(ko, request) {
         // Set displayName
         displayName: 'User Settings',
 
-        // On activate, get info...
-        activate: function () {
-          this.getSettings();
-        },
-
         // Setup model
         fname: ko.observable(),
         lname: ko.observable(),
@@ -23,27 +18,23 @@ function(ko, request) {
         password: ko.observable(),
         confirm_password: ko.observable(),
 
+        // On activate, get info...
+        activate: function (context) {
+          this.getSettings(context);
+        },
+
         // Define get-info request object
         getSettingsRequest: {
-          url: '/api/user',
-          type: 'GET',
-          done: function (data) {
-            this.fname(data.fname);
-            this.lname(data.lname);
-            this.email(data.email);
-            this.username(data.username);
-          },
-          fail: function (err) {
-            this.fname('NO DATA');
-            this.lname('NO DATA');
-            this.email('NO DATA');
-            this.username('NO DATA');
-          }
+          url: '/users',
+          type: 'GET'
         },
 
         // Request info to populate model
-        getSettings: function (id) {
-          request(this.getSettingsRequest, { _id: id });
+        getSettings: function (user) {
+          var req = request(this.getSettingsRequest, { 'user': user });
+          req.done(function (data) {
+            console.log(data);
+          });
         },
 
         // Define save-info request object
@@ -51,7 +42,7 @@ function(ko, request) {
           url: '/api/user',
           type: 'PUT',
           done: function (data) {
-            console.log(data);
+            return data;
           },
           fail: function (err) {
             console.log(err);
--- assets/js/utils/request.js
+++ assets/js/utils/request.js
@@ -1,14 +1,20 @@
 define(['jquery'], function ($) {
 
+  'use strict';
+
   // Just a simple handler to abstract $.ajax calls
   // from the controllers
   var request = function (req, payload) {
-    $.ajax({
+    return $.ajax({
       url: req.url,
       type: req.type,
       data: payload || {},
-      success: req.done,
-      error: req.fail
+      success: function () {
+        return this;
+      },
+      error: function () {
+        return this;
+      }
     });
   };
 
