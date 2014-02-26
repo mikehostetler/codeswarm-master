5c5cacc68643029dd4715694100dbd2b72060799
fluidbyte <kent@fluidbyte.net>
2014-02-26 02:13:46 +0000

    Setup core for data binding, vm and controller methods for user/NAME/settings

--- assets/js/codeswarm/user/settings.js
+++ assets/js/codeswarm/user/settings.js
@@ -1,12 +1,82 @@
-﻿define(function() {
-    var ctor = function () {
-        this.displayName = 'Settings';
+define([
+  'knockout',
+  'request'
+],
+
+function(ko, request) {
+
+    var ctor = {
+
+        // Set displayName
+        displayName: 'User Settings',
+
+        // On activate, get info...
+        activate: function () {
+          this.getSettings();
+        },
+
+        // Setup model
+        fname: ko.observable(),
+        lname: ko.observable(),
+        email: ko.observable(),
+        username: ko.observable(),
+        password: ko.observable(),
+        confirm_password: ko.observable(),
+
+        // Define get-info request object
+        getSettingsRequest: {
+          url: '/api/user',
+          type: 'GET',
+          done: function (data) {
+            this.fname(data.fname);
+            this.lname(data.lname);
+            this.email(data.email);
+            this.username(data.username);
+          },
+          fail: function (err) {
+            this.fname('NO DATA');
+            this.lname('NO DATA');
+            this.email('NO DATA');
+            this.username('NO DATA');
+          }
+        },
+
+        // Request info to populate model
+        getSettings: function (id) {
+          request(this.getSettingsRequest, { _id: id });
+        },
+
+        // Define save-info request object
+        saveSettingsRequest: {
+          url: '/api/user',
+          type: 'PUT',
+          done: function (data) {
+            console.log(data);
+          },
+          fail: function (err) {
+            console.log(err);
+          }
+        },
+
+        // Save settings handler method
+        trySaveSettings: function () {
+          // Ensure password match
+          if (this.password() === this.confirm_password()) {
+            // Define request payload
+            var payload = {
+              'fname': this.fname(),
+              'lname': this.lname(),
+              'email': this.email(),
+              'username': this.username(),
+              'password': this.password()
+            };
+            // Processes request obj
+            request(this.saveSettingsRequest, payload);
+          } else {
+            alert("Your passwords don't match.");
+          }
+        }
+
     };
-
-    //Note: This module exports a function. That means that you, the developer, can create multiple instances.
-    //This pattern is also recognized by Durandal so that it can create instances on demand.
-    //If you wish to create a singleton, you should export an object instead of a function.
-    //See the "flickr" module for an example of object export.
-
     return ctor;
 });
