55680289a9f1d1f083154669f523297074c95d93
Mike Hostetler <mike.hostetler@gmail.com>
2014-02-27 15:04:49 -0600

    App updates

--- assets/js/codeswarm/app.js
+++ assets/js/codeswarm/app.js
@@ -18,12 +18,12 @@ define(['plugins/router', 'durandal/app', 'dom'], function (router, app, dom) {
                 { route: 'search/*term',		moduleId: 'codeswarm/home/search',				title: 'Search',						nav: true },
 
                 // User Management
+                { route: 'user',						moduleId: 'codeswarm/user/index',					title: 'User Profile',			nav: true },
                 { route: 'user/login',			moduleId: 'codeswarm/user/login',					title: 'Login',							nav: true },
                 { route: 'user/logout',			moduleId: 'codeswarm/user/logout',				title: 'Logout',						nav: true },
-                { route: 'user/forgot-password',	moduleId: 'codeswarm/user/forgot-password',											nav: true },
+                { route: 'user/forgot',			moduleId: 'codeswarm/user/forgot-password',											nav: true },
                 { route: 'user/signup',			moduleId: 'codeswarm/user/signup',				title: 'Signup',						nav: true },
-                { route: ['user',':user'],	moduleId: 'codeswarm/user/index',					title: 'User Profile',			nav: true },
-                { route: 'user/:user/settings',	moduleId: 'codeswarm/user/settings',			title: 'User Settings',			nav: true },
+                { route: 'user/settings',		moduleId: 'codeswarm/user/settings',			title: 'User Settings',			nav: true },
 
 								// Orgs
                 { route: ':org',						moduleId: 'codeswarm/org/index',					nav: true },
