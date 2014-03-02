a9ce5fbf62212c54a1421b0e77d2adcd96d9d0ee
fluidbyte <kent@fluidbyte.net>
2014-03-02 18:49:19 +0000

    Cleaning up js/main formatting

--- www/js/main.js
+++ www/js/main.js
@@ -1,39 +1,42 @@
+/* global requirejs:true */
 requirejs.config({
-    paths: {
-        'text': 'vendor/requirejs-text/text',
-        'durandal':'vendor/durandal',
-        'plugins' : 'vendor/durandal/plugins',
-        'knockout': 'vendor/knockout/knockout-2.3.0',
-        'jquery': 'vendor/jquery/jquery',
-				'ansi_up': 'vendor/ansi_up/ansi_up',
-				'github': 'vendor/github/github',
-				'underscore': 'vendor/underscore/underscore-min',
-				'request': 'utils/request',
-				'dom': 'utils/dom',
-				'session': 'utils/session'
-    }
+  paths: {
+    'text': 'vendor/requirejs-text/text',
+    'durandal':'vendor/durandal',
+    'plugins' : 'vendor/durandal/plugins',
+    'knockout': 'vendor/knockout/knockout-2.3.0',
+    'jquery': 'vendor/jquery/jquery',
+    'ansi_up': 'vendor/ansi_up/ansi_up',
+    'github': 'vendor/github/github',
+    'underscore': 'vendor/underscore/underscore-min',
+    'request': 'utils/request',
+    'dom': 'utils/dom',
+    'session': 'utils/session'
+  }
 });
 
-define(['durandal/system', 'durandal/app', 'durandal/viewLocator' ],  function (system, app, viewLocator) {
+define([
+  'durandal/system',
+  'durandal/app',
+  'durandal/viewLocator'
+], function (system, app, viewLocator) {
 
-    //>>excludeStart("build", true);
-    system.debug(true);
-    //>>excludeEnd("build");
+  // Set debug
+  system.debug(true);
 
-    app.title = 'Welcome to CodeSwarm!';
+  // Application title
+  app.title = 'Welcome to CodeSwarm!';
 
-    app.configurePlugins({
-        router: true,
-				widget: true
-    });
+  // Plugins
+  app.configurePlugins({
+    router: true,
+    widget: true
+  });
 
-    app.start().then(function() {
-			//Replace 'viewmodels' in the moduleId with 'views' to locate the view.
-			//Look for partial views in a 'views' folder in the root.
-			viewLocator.useConvention();
-
-			//Show the app by setting the root view model for our application with a transition.
-			//app.setRoot('viewmodels/shell', 'entrance');
-			app.setRoot('codeswarm/app');
-    });
+  app.start().then(function() {
+    // Define viewLocator convention
+    viewLocator.useConvention();
+    // Set app root
+    app.setRoot('codeswarm/app');
+  });
 });
