df4b93693257b6b69d53142932cb5b418c7fc6d7
fluidbyte <kent@fluidbyte.net>
2014-03-03 14:22:11 +0000

    Moving socketio ref into main w shim, remove extra underscore call

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,12 +7,18 @@ requirejs.config({
     'transitions': 'vendor/durandal/transitions',
     'knockout': 'vendor/knockout/knockout-2.3.0',
     'jquery': 'vendor/jquery/jquery',
+    'socketio': 'socket.io.js',
     'ansi_up': 'vendor/ansi_up/ansi_up',
     'github': 'vendor/github/github',
     'underscore': 'vendor/underscore/underscore-min',
     'request': 'utils/request',
     'dom': 'utils/dom',
     'session': 'utils/session'
+  },
+  shim: {
+    'socketio': {
+      exports: 'io'
+    }
   }
 });
 
