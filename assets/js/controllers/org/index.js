c82aad4b4c35d7e94b09d62f414e06b793812094
fluidbyte <kent@fluidbyte.net>
2014-03-16 21:43:02 +0000

    Fixing list view to transition pages instead of fully navigate and 'reload'

--- assets/js/controllers/org/index.js
+++ assets/js/controllers/org/index.js
@@ -89,6 +89,7 @@ define([
         var org;
         for (var i = 0, z = data.length; i < z; i++) {
           org = data[i]._id.substr(0, data[i]._id.indexOf('/')).toLowerCase();
+          data[i].project_url = '#'+data[i]._id;
           if (self.org() === 'projects') {
             self.projects.push(data[i]);
           } else {
@@ -99,8 +100,8 @@ define([
         }
         // Sort
         self.projects.sort(function(left, right) {
-          return (left._id.toLowerCase() === right._id.toLowerCase()) 
-            ? 0 
+          return (left._id.toLowerCase() === right._id.toLowerCase())
+            ? 0
             : (left._id.toLowerCase() < right._id.toLowerCase() ? -1 : 1);
         });
       });
