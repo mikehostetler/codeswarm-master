5b092997086d3b913f2054afcc1f923c919c7b5a
Pedro Teixeira <i@pgte.me>
2014-03-28 22:09:16 +0000

    initializing models

--- bin/fetch_pull_requests.js
+++ bin/fetch_pull_requests.js
@@ -1,63 +1,36 @@
 #!/usr/bin/env node
 
-var extend = require('util')._extend;
+var bootstrap = require('./bootstrap');
 
-var Waterline = require('../node_modules/sails/node_modules/waterline');
+bootstrap(['Project', 'User'], fetchOne);
 
-var couchdb = require('sails-couchdb-orm');
+var projectIndex = 0;
+var pageSize = 1;
 
-var config = {
-  username: 'admin',
-  password: 'admin'
-};
-
-couchdb.config = config;
-
-var collConfig = {
-  adapter: 'couchdb',
-  config: config
-};
-console.log('collection config:', collConfig);
-
-var Project = Waterline.Collection.extend(require('../api/models/Project'), collConfig);
-
-var modelConfig = {
-  adapters: { 'couchdb': couchdb }
-};
-
-new Project(modelConfig, gotProjectModel);
-
-function gotProjectModel(err, Project) {
-
-  console.log(Project.adapter);
-  var projectIndex = 0;
-  var pageSize = 1;
-
-  fetch();
-
-  return;
-
-
-  function fetch() {
-    Project.find().limit(pageSize).skip(projectIndex).exec(foundProjects);
-  }
-
-  function foundProjects(err, projects) {
-    if (err) return ended(err);
-    if (! projects.length) return ended();
-    if (projects.length > pageSize) throw new Error('got more than ' + pageSize + ' projects');
-    var project = projects[0];
-    console.log('Project:' + project.id);
-    fetch();
-  }
-
-  function ended(err) {
-    if (err) {
-      console.error('ended with error: ' + err.message);
-      console.error('\n' + err.stack);
-      process.exit(err.code || 1);
-      return;
-    } else console.log('ended.');
-  }
+function fetchOne() {
+  console.log('fetch %d', projectIndex);
+  Project.find().limit(pageSize).skip(projectIndex).exec(foundProject);
 }
 
+function foundProject(err, projects) {
+  console.log('found');
+  if (err) return ended(err);
+  if (! projects.length) return ended();
+  if (projects.length > pageSize) throw new Error('got more than ' + pageSize + ' projects');
+  var project = projects[0];
+  console.log('Project:' + project.id);
+  projectIndex ++;
+
+
+
+  fetchOne();
+}
+
+function ended(err) {
+  if (err) {
+    console.error('ended with error: ' + err.message);
+    console.error('\n' + err.stack);
+    process.exit(err.code || 1);
+    return;
+  } else console.log('ended.');
+}
