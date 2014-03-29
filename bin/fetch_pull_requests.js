bab18ca77557c8a9d8addaedfc209eb290a787f5
Pedro Teixeira <i@pgte.me>
2014-03-29 10:43:31 +0000

    saving pull requests

--- bin/fetch_pull_requests.js
+++ bin/fetch_pull_requests.js
@@ -1,14 +1,61 @@
 #!/usr/bin/env node
 
+var async     = require('async');
 var bootstrap = require('./bootstrap');
+var github    = require('../lib/github');
 
-bootstrap(['Project', 'User'], fetchOne);
+bootstrap(['Project', 'User', 'PullRequest'], next);
 
-var projectIndex = 0;
+var projectIndex = 0-1;
 var pageSize = 1;
 
+function process(project, cb) {
+  if (project.id.charAt(0) == '_') return cb();
+
+  console.log('project opwners:', project);
+  async.map(project.owners || [], getUserGithubKey, gotOwnerKeys);
+
+  function gotOwnerKeys(err, tokens) {
+    if (err) return cb(err);
+
+    var token = tokens.filter(exists)[0];
+    if (! token) return cb(new Error('no github token found for project ' + project.id));
+    else fetchPullRequests(project.id, token.token, gotPullRequests);
+  }
+
+  function gotPullRequests(err, pullRequests) {
+    if (err) cb(err);
+    else async.eachSeries(pullRequests, insertPullRequest, cb);
+  }
+
+  function insertPullRequest(pullRequest, cb) {
+    PullRequest.create({
+      id: pullRequest.id,
+      project: project.id,
+      github_data: pullRequest
+    }, cb);
+  }
+}
+
+
+function fetchPullRequests(project, token, cb) {
+  github.getPullRequests(project, token, cb);
+}
+
+
+function getUserGithubKey(user, cb) {
+  User.tokenFor(user, 'github', function(err, key) {
+    if (err) cb(err);
+    else cb(null, key);
+  });
+}
+
+function next() {
+  projectIndex ++;
+  fetchOne();
+}
+
 function fetchOne() {
-  console.log('fetch %d', projectIndex);
   Project.find().limit(pageSize).skip(projectIndex).exec(foundProject);
 }
 
@@ -21,9 +68,12 @@ function foundProject(err, projects) {
   console.log('Project:' + project.id);
   projectIndex ++;
 
+  process(project, processed);
 
-
-  fetchOne();
+  function processed(err) {
+    if (err) console.error(err.stack);
+    next();
+  }
 }
 
 function ended(err) {
@@ -33,4 +83,11 @@ function ended(err) {
     process.exit(err.code || 1);
     return;
   } else console.log('ended.');
+}
+
+
+/// Misc
+
+function exists(o) {
+  return !! o;
 }
