75ecf51d7e6bed2494d17ba22488266000b97a42
Mike Hostetler <mike.hostetler@gmail.com>
2014-04-15 16:11:47 -0500

    Cleaning up logs and adding some flair

---
+++ api/db/index.js
@@ -0,0 +1,19 @@
+var nano = require('nano');
+var config = require('../../config/couchdb');
+
+var base = exports.base = config.url || 'http://localhost:5984';
+
+exports.public     = nano(base);
+exports.privileged = require('./privileged');
+exports.session    = require('./session');
+exports.wrap       = wrap;
+
+function wrap(options) {
+  if (typeof options != 'object') {
+    options = {
+      url: options
+    };
+  }
+
+  return nano(options);
+}
