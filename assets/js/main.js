3bb0f42aaea7e35c99e882227d89acff5dae9635
Mike Hostetler <mike.hostetler@gmail.com>
2014-04-22 14:07:41 -0500

    Reverting deletion of auth.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,66 +1,52 @@
-require.config({
-	baseUrl: 'js',
-	paths: {
-		'jquery': 'vendor/jquery/jquery.min',
-		'text': 'vendor/requirejs-text/text',
-		'handlebars': 'vendor/handlebars/handlebars.min',
-		'knockout': 'vendor/knockout/knockout-3.1.0',
-		'durandal': 'vendor/durandal/js',
-		'plugins': 'vendor/durandal/js/plugins',
-		'ansi_up': 'vendor/ansi_up/ansi_up',
-		'github': 'vendor/github/github',
-		'base64': 'vendor/base64/base64',
-		'async': 'vendor/async/async',
-		'underscore': 'vendor/underscore/underscore',
-        'controllers': 'controllers',
-        'routes': 'routes'
-	},
-	shim: {
-		'handlebars': {
-			'exports': 'Handlebars'
-		}
-	}
-});
+require(['require-config'], function() {
+	require([
+		'durandal/system', 
+		'durandal/app', 
+		'durandal/viewLocator', 
+		'plugins/router', 
+		'controllers/session', 
+		'controllers/socket', 
+		'controllers/dom'
+	], function (system, app, viewLocator, router, session, socket, dom) {
 
-define(['durandal/system', 'durandal/app', 'plugins/router', 'controllers/session', 'controllers/socket', 'controllers/dom'], function (system, app, router, session, socket, dom) {
+		system.debug(true);
 
     app.configurePlugins({
         router: true
     });
 
-    app.start().then(
-        function () {
+		app.title = 'Welcome to CodeSwarm!';
 
-            var routings;
+    app.start().then(function () {
+				var routing;
+				socket.reset();
+				dom.init();
 
-            system.debug(true);
+				routing = {
+					router: router,
+					activate: function () {
+						router.map([
+							{ route: '', title: 'Home', moduleId: 'routes/login_route', nav: true },
+							{ route: 'projects', title: 'Projects', moduleId: 'routes/projects_route', nav: true },
+							{ route: ':owner/:repo', title: 'Repo', moduleId: 'routes/repo_route', nav: true },
+							{ route: ':owner/:repo/config', title: 'Repo', moduleId: 'routes/repo_config_route', nav: true },
+							{ route: ':owner/:repo/plugins', title: 'Repo', moduleId: 'routes/repo_plugins_route', nav: true },
+							{ route: ':owner/:repo/builds', title: 'Repo', moduleId: 'routes/repo_builds_route', nav: true },
+							{ route: ':owner/:repo/builds/:log', title: 'Repo', moduleId: 'routes/repo_log_route', nav: true },
+							{ route: 'project/new', title: 'Repo', moduleId: 'routes/new_project_route', nav: true },
+							{ route: 'logout', title: 'Repo', moduleId: 'routes/logout_route', nav: true },
+							{ route: 'users/new', title: 'Repo', moduleId: 'routes/new_user_route', nav: true },
+						]).buildNavigationModel();
+		 
+						return router.activate();
+					}
+				};
 
-            socket.reset();
+				routing.activate();
 
-            dom.init();
-
-            routings = {
-                router: router,
-                activate: function () {
-                    router.map([
-                        { route: '', title: 'Home', moduleId: 'routes/login_route', nav: true },
-                        { route: 'projects', title: 'Projects', moduleId: 'routes/projects_route', nav: true },
-                        { route: ':owner/:repo', title: 'Repo', moduleId: 'routes/repo_route', nav: true },
-                        { route: ':owner/:repo/config', title: 'Repo', moduleId: 'routes/repo_config_route', nav: true },
-                        { route: ':owner/:repo/plugins', title: 'Repo', moduleId: 'routes/repo_plugins_route', nav: true },
-                        { route: ':owner/:repo/builds', title: 'Repo', moduleId: 'routes/repo_builds_route', nav: true },
-                        { route: ':owner/:repo/builds/:log', title: 'Repo', moduleId: 'routes/repo_log_route', nav: true },
-                        { route: 'project/new', title: 'Repo', moduleId: 'routes/new_project_route', nav: true },
-                        { route: 'logout', title: 'Repo', moduleId: 'routes/logout_route', nav: true },
-                        { route: 'users/new', title: 'Repo', moduleId: 'routes/new_user_route', nav: true },
-                    ]).buildNavigationModel();
-         
-                    return router.activate();
-                }
-            };
-
-            routings.activate();
-        });
+				// Define viewLocator convention
+				viewLocator.useConvention('controllers', 'views');
+		});
 
     app.go2 = function (fragment) {
         router.navigate(fragment);
@@ -69,4 +55,5 @@ define(['durandal/system', 'durandal/app', 'plugins/router', 'controllers/sessio
     window.addEventListener('hashchange', function () {
         router.compositionComplete();
     }, false);
+	});
 });
