b52d648b71095d3136c8e5d79356bfa2b43de638
William J. Edney <bedney@technicalpursuit.com>
2014-05-09 10:17:40 -0500

    Initial commit of project table - needs CSS love.

--- assets/js/controllers/home/index.js
+++ assets/js/controllers/home/index.js
@@ -1,17 +1,38 @@
 define([
-  'utils/session'
-], function (session) {
+    'models/user',
+    'models/projects',
+    'utils/session',
+    'knockout'
+  ],
+
+  function (user, projects, session, ko) {
 
   return {
-    displayName: 'Welcome',
 
-    activate: function () {
-			/*
-      session.data(function () {
-        console.log(arguments);
-      });
-			*/
-    }
+			/**
+			 * local viewmodel properties
+			 */
 
-  };
+            displayName: 'Welcome',
+            isLoggedIn: ko.observable(),
+
+            projects: ko.observableArray([]),
+ 
+			/**
+			 * Activate our model, this method is always called
+			 */
+            activate: function () {
+                user.isLoggedIn(
+                        function (res) {
+                            this.isLoggedIn(res);
+                        }.bind(this));
+
+                amplify.subscribe('projects', this, function (stuff) {
+                    this.projects.push.apply(this.projects, stuff);
+                });
+
+                projects.getAllProjects();
+            },
+        };
+
 });
