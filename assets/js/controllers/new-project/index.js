2ec15ca2ddf2d3b71296979ee4dd650ff29aa90c
Mike Hostetler <mike.hostetler@gmail.com>
2014-05-19 15:41:22 -0500

    Cleaned up controllers and working on the new project route

--- assets/js/controllers/new-project/index.js
+++ assets/js/controllers/new-project/index.js
@@ -2,30 +2,57 @@ define([
   'durandal/app',
   'plugins/router',
   'knockout',
-	'models/user'
-], function (app, router, ko, user) {
-	var router = router.createChildRouter()
+	'models/user',
+	'models/projects'
+], function (app, router, ko, User, Project) {
+/*
+	var childRouter = router.createChildRouter()
 				.map([
 					{ route: 'new/source', moduleId: 'controllers/new-project/choose-source', title: 'Choose Project Source', nav: true},
 					{ route: 'new/repo/:source', moduleId: 'controllers/new-project/choose-repo', title: 'Choose Project Repository', nav: true},
 					{ route: 'new/type', moduleId: 'controllers/new-project/choose-type', title: 'Choose Project Type', nav: true}
 
 				]).buildNavigationModel();
+				*/
 
   return {
     router: router,
 
-		newVar: 'foo',
+		strRepository: ko.observable().extend({required: true}),
+		strBranch: ko.observable().extend({required: true}),
+		arrTypes: ko.observableArray(['node','browser','custom']),
+		strChosenType: ko.observableArray(['']),
+		blnPublic: ko.observable(false),
 
 		/**
 		 * Activate our model, this method is always called
 		 */
 		activate: function() {
 			// If session active, go home
-			user.isLoggedIn(function(isLoggedIn) {
+			User.isLoggedIn(function(isLoggedIn) {
 				if(isLoggedIn === false) {
-					alert(isLoggedIn);
-					//router.navigate('user/login');
+					router.navigate('user/login');
+				}
+			});
+		},
+
+		frmNewProject_Submit: function () {
+
+			var currentUser = User.getCurrentUser(),
+				newProject = new Project({
+													repo: this.strRepository(),
+													branch: this.strBranch(),
+													public: (this.blnPublic == true?true:false),
+													type: this.strChosenType(),
+													owners: [currentUser.username]
+												});
+
+			newProject.save({ 
+				success:function(result) {
+					console.log('We got a result!',result);
+				},
+				error:function(err) {
+					console.log('We got an error!',err);
 				}
 			});
 		},
