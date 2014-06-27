b8b4cc76cbb332a4981901731a91ba6c7cc816c6
Mike Hostetler <mike.hostetler@gmail.com>
2014-06-27 10:51:54 -0500

    Removed extra console.log statement

--- api/policies/userOwnsProjectOrPublic.js
+++ api/policies/userOwnsProjectOrPublic.js
@@ -3,10 +3,8 @@ module.exports = userOwnsProject;
 function userOwnsProject(req, res, next) {
   var user = req.user.username;
 	var id = req.param('project-id');	
-	console.log("Checking whether user owns the project or public for Project Id: ",id);
 
   Project.findOne({id: id}, replied);
-
   function replied(err, project) {
     if (err) res.send(err.status_code || 500, err);
     else if (! project) res.send(404, new Error('Project not found'));
